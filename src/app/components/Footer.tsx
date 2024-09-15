e25088fba856a933d80b845ad9a5d346ffe54eb1
m3di0cre3 <anandtandon8@gmail.com>
2024-09-15 13:57:01 -0400

    desktop sussy commit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -15,10 +15,10 @@ export const Footer:React.FC = () => {
             <nav>
                 <h6 className="footer-title">Social</h6>
                 <div className="grid grid-flow-col gap-4 !pt-0.5">
-                    <a>
+                    <a href="https://www.instagram.com/anand_t_photography/" target="_blank">
                         <img className="object-contain" src={InstagramLogo.src}/>
                     </a>
-                    <a>
+                    <a href="mailto:anandtandon8@gmail.com" target="_blank">
                         <img className="object-contain" src={MailLogo.src}/>
                     </a>
                 </div>
@@ -30,14 +30,14 @@ export const Footer:React.FC = () => {
                 <form>
                     <h6 className="footer-title">Newsletter</h6>
                     <fieldset className="form-control w-80">
-                        <label className="label !pt-0">
+                        <label className="label pt-0 pl-0">
                             <span className="label-text">Enter your email address</span>
                         </label>
                         <div className="join">
                             <input
                                 type="text"
                                 placeholder="username@site.com"
-                                className="input input-bordered join-item focus:outline-none bg-neutral-50 focus:bg-white" />
+                                className="input input-bordered join-item text-base bg-white" />
                             <button className="btn btn-primary join-item bg-neutral-700 hover:bg-neutral-500 btn-ghost text-white">Subscribe</button>
                         </div>
                     </fieldset>
