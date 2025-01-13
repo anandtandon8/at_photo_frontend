87684f7232a8deef90a255f66d3b6d9247f4a18b
m3di0cre3 <anandtandon8@gmail.com>
2025-01-12 23:10:30 -0500

    didnt commit bruh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -2,6 +2,6 @@
 cd /var/www/html/atphoto
 
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 941377117805.dkr.ecr.us-east-2.amazonaws.com
-docker pull 941377117805.dkr.ecr.us-east-2.amazonaws.com/anandtandon8/atphoto:latest
-docker tag 941377117805.dkr.ecr.us-east-2.amazonaws.com/anandtandon8/atphoto:latest atphoto:latest 
+docker pull 941377117805.dkr.ecr.us-east-2.amazonaws.com/anandtandon8/atphoto:Latest
+docker tag 941377117805.dkr.ecr.us-east-2.amazonaws.com/anandtandon8/atphoto:Latest atphoto:Latest 
 
