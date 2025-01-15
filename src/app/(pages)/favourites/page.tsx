6c8f1efbe3480a5b393ef4c1de8dbb4f669e3b6c
m3di0cre3 <anandtandon8@gmail.com>
2025-01-15 03:00:49 -0500

    updated favourites

--- src/app/(pages)/favourites/page.tsx
+++ src/app/(pages)/favourites/page.tsx
@@ -4,54 +4,32 @@ import Contact from "@/app/components/Contact"
 import Footer from "@/app/components/Footer"
 import GalleryFormat from "@/app/components/GalleryFormat";
 
-import waterfall from '@/app/assets/img/IMG_2122.jpg';
-import sunset from '@/app/assets/img/IMG_2328.jpg';
-import mountain from '@/app/assets/img/IMG_2490.jpg';
-import car from '@/app/assets/img/IMG_4851.jpg';
-import cnTower from '@/app/assets/img/IMG_4885.jpg';
+import fs from 'fs';
+import path from 'path';
 
 
-const favouriteImages = [
-  {
-    src: waterfall.src,
-    alt: 'waterfall',   // get alt from jsonl
-    width: waterfall.width,
-    height: waterfall.height,
-    blurDataURL: waterfall.blurDataURL
-  },
+interface GalleryImage {
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+  blurDataURL?: string;
+}
 
-  {
-    src: sunset.src,
-    alt: 'sunset',
-    width: sunset.width,
-    height: sunset.height,
-    blurDataURL: sunset.blurDataURL
-  },
 
-  {
-    src: mountain.src,
-    alt: 'mountain',
-    width: mountain.width,
-    height: mountain.height,
-    blurDataURL: mountain.blurDataURL
-  },
-
-  {
-    src: car.src,
-    alt: 'car',
-    width: car.width,
-    height: car.height,
-    blurDataURL: car.blurDataURL
-  },
-
-  {
-    src: cnTower.src,
-    alt: 'cnTower',
-    width: cnTower.width,
-    height: cnTower.height,
-    blurDataURL: cnTower.blurDataURL
-  },
-];
+const favouriteImagesDirectory = path.join(process.cwd(), 'src/app/assets/img/Favourites')
+const favouriteImages: GalleryImage[] = fs.readdirSync(favouriteImagesDirectory)
+  .filter(file => /\.(jpg|jpeg|png)$/i.test(file))
+  .map(file => {
+    const image = require(`@/app/assets/img/Favourites/${file}`).default
+    return {
+      src: image.src,
+      alt: path.basename(file, path.extname(file)),
+      width: image.width,
+      height: image.height,
+      blurDataURL: image.blurDataURL
+    }
+  })
 
 
 export default function Favourites() {
