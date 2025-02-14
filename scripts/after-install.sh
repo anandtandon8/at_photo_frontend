2a4269aabac71b1afe3546bc88386dc9e979ea28
m3di0cre3 <anandtandon8@gmail.com>
2025-02-14 05:26:34 -0500

    fixed docker pull flags

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -7,6 +7,6 @@ sudo docker rmi atphoto:Latest || true
 sudo docker rmi 941377117805.dkr.ecr.us-east-2.amazonaws.com/anandtandon8/atphoto:Latest || true
 
 sudo aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 941377117805.dkr.ecr.us-east-2.amazonaws.com
-sudo docker pull --no-cache 941377117805.dkr.ecr.us-east-2.amazonaws.com/anandtandon8/atphoto:Latest
+sudo docker pull 941377117805.dkr.ecr.us-east-2.amazonaws.com/anandtandon8/atphoto:Latest
 sudo docker tag 941377117805.dkr.ecr.us-east-2.amazonaws.com/anandtandon8/atphoto:Latest atphoto:Latest 
 
